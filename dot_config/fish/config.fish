1df8e3a323870978978fa7571586f78416bf44bd
Gustavo <gu.does.dev@gmail.com>
2022-05-30 22:43:46 -0300

    removing spark from cls on FISH config

--- dot_config/fish/config.fish
+++ dot_config/fish/config.fish
@@ -190,8 +190,8 @@ end
 # \x1b[2J   <- clears tty
 # \x1b[1;1H <- goes to (1, 1) (start)
 alias spicetify="/home/personal/spicetify-cli/spicetify"
-alias clear='echo -en "\x1b[2J\x1b[1;1H" ; seq 1 (tput cols) | sort -R | spark | lolcat; PF_INFO="ascii title os uptime memory wm shell palette" pfetch'
-alias cls='echo -en "\x1b[2J\x1b[1;1H" ; seq 1 (tput cols) | sort -R | spark | lolcat; PF_INFO="ascii title os uptime memory wm shell palette" pfetch'
+alias clear='echo -en "\x1b[2J\x1b[1;1H" ; PF_INFO="ascii title os uptime memory wm shell palette" pfetch'
+alias cls='echo -en "\x1b[2J\x1b[1;1H" ; PF_INFO="ascii title os uptime memory wm shell palette" pfetch'
 
 # root privileges
 alias doas="doas --"
